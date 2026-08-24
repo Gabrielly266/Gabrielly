ec8518f071483809c810b3f0624f27ea1307ea14
Gabrielly266 <96848602+Gabrielly266@users.noreply.github.com>
2026-08-23 23:00:18 -0300

    Melhora desempenho, organização e tratamento de entradas

--- projetos/algoritmos-c/fibonacci.c
+++ projetos/algoritmos-c/fibonacci.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
-#ifdef _WIN32
-#include <windows.h>
-#endif
 
-int fibonacci(int n){
-    if (n==0){
-        return 0;
+#define MAIOR_TERMO 46
+
+int fibonacci(int n, int memoria[]) {
+    if (n < 2) {
+        return n;
     }
-    if (n==1){
-        return 1;
+
+    if (memoria[n] == -1) {
+        memoria[n] = fibonacci(n - 1, memoria) + fibonacci(n - 2, memoria);
     }
-    return fibonacci (n-1) + fibonacci(n-2);
+
+    return memoria[n];
 }
 
-int main() {
-    int n, f;
-    #ifdef _WIN32
-    SetConsoleOutputCP(65001);
-    #endif
-    printf("Escreva um valor para n:");
-    if (scanf("%d", &n) != 1 || n < 0 || n > 46) {
-        printf("Digite um número entre 0 e 46.\n");
+int main(void) {
+    int n;
+    int memoria[MAIOR_TERMO + 1];
+
+    for (int i = 0; i <= MAIOR_TERMO; i++) {
+        memoria[i] = -1;
+    }
+
+    printf("Posição na sequência de Fibonacci: ");
+    if (scanf("%d", &n) != 1 || n < 0 || n > MAIOR_TERMO) {
+        fprintf(stderr, "Informe um número entre 0 e %d.\n", MAIOR_TERMO);
         return 1;
     }
-    f=fibonacci(n);
-    printf("%d", f);
+
+    printf("Fibonacci(%d) = %d\n", n, fibonacci(n, memoria));
     return 0;
 }
